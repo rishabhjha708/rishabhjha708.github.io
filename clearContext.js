142e9cc4dd269f9a3bc9503148e46748956ee2dc
Rishabh Jha <13484708+rishabhjha708@users.noreply.github.com>
2021-06-30 22:40:48 +0530

    Add files via upload

---
+++ clearContext.js
@@ -0,0 +1,24 @@
+console.log("custom script");
+window.addEventListener('message', function (eventData) {
+    // console.log("Event catched");
+    try {
+        if (JSON.parse(eventData.data)) {
+            let event = JSON.parse(eventData.data);
+            // console.log(event, "testing")
+            if (event.data && event.data.code === "clear") {
+                console.log("clear context event catched")
+                console.log(document);
+                const innerIframe = document.getElementById("ymIframe");
+                innerIframe.contentDocument.getElementById("chatBoxMain").innerHTML = "";
+                innerIframe.contentDocument.getElementById("chatOptions").innerHTML = "";
+
+                window.YellowMessengerPlugin.sendEvent(JSON.stringify({
+                    event_code: 'ym-client-event', data: "start"
+                }), '*');
+            }
+            return;
+        }
+    } catch (error) {
+        console.log(error, "error")
+    }
+}, false);
