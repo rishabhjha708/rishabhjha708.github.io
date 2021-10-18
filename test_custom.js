f69bd932e07461938152fd94b087c75e33fda1c4
Rishabh Jha <13484708+rishabhjha708@users.noreply.github.com>
2021-10-18 21:46:45 +0530

    Update test_custom.js

--- test_custom.js
+++ test_custom.js
@@ -9,21 +9,23 @@ window.addEventListener(
         // console.log(event, "testing")
         if (event.data && event.data.code === "clear") {
           const innerIframe = document.getElementById("ymIframe");
-          innerIframe.contentDocument.getElementById("chatBoxMain").innerHTML = "";
-          innerIframe.contentDocument.getElementById("chatOptions").innerHTML = "";
+          innerIframe.contentDocument.getElementById("chatBoxMain").innerHTML =
+            "";
+          innerIframe.contentDocument.getElementById("chatOptions").innerHTML =
+            "";
 
-//           window.YellowMessengerPlugin.sendEvent(
-//             JSON.stringify({
-//               event_code: "ym-client-event",
-//               data: "start",
-//             }),
-//             "*"
-//           );
-        } 
-        else if (event.data && event.data.code === "start-chat") {
+          //           window.YellowMessengerPlugin.sendEvent(
+          //             JSON.stringify({
+          //               event_code: "ym-client-event",
+          //               data: "start",
+          //             }),
+          //             "*"
+          //           );
+        } else if (event.data && event.data.code === "start-chat") {
           try {
-            const usrInput = document.getElementById("ymMsgInput");
-            console.log("here->", usrInput)
+            const innerIframe = document.getElementById("ymIframe");
+            const usrInput = innerIframe.contentDocument.getElementById("ymMsgInput");
+            console.log("here->", usrInput);
             usrInput.addEventListener("input", function (data) {
               console.log("data-> ", data.target.value);
             });
