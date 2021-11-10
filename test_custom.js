2736d127d514555f3125f989f08f56560dbaeaa9
Rishabh Jha <13484708+rishabhjha708@users.noreply.github.com>
2021-11-10 10:49:34 +0530

    Update test_custom.js

--- test_custom.js
+++ test_custom.js
@@ -95,41 +95,8 @@ window.addEventListener(
             console.log(err);
           }
         } else if (event.event_code == "ym-bot-opened") {
-          window.ymInterval = setInterval(() => {
-            try {
-              console.log("value triggered for event", event.event_code);
-              const frameDocument =
-                window.frames &&
-                window.frames.ymIframe &&
-                window.frames.ymIframe.document;
-              const title = frameDocument.getElementsByClassName("title");
-              if (title) {
-                title[0].remove();
-              }
-              const subTitle =
-                frameDocument.getElementsByClassName("sub-title");
-              if (subTitle) {
-                subTitle[0].remove();
-              }
-              var x = frameDocument.getElementsByClassName("title-parent");
-              console.log(" title execution parent", x);
-              var element = frameDocument.createElement("img");
-              element.setAttribute("class", "icon1");
-              element.style.maxWidth = "200px";
-              element.style.margin = "-10px 0 0 -25px";
-              element.style.padding = 0;
-              element.setAttribute(
-                "src",
-                "https://cdn.yellowmessenger.com/Eaf9VXBO8C661636518084435.png"
-              );
-              if (x) {
-                x[0].appendChild(element);
-              }
-              clearInterval(window.ymInterval);
-            } catch (error) {
-              clearInterval(window.ymInterval);
-            }
-          }, 100);
+          // pass
+          return;
         }
       }
     } catch (error) {
